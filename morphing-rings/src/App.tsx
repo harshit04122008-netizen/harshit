9496a1563168fa0a0b53a98b4e165e25f0f94d49
harshit04122008-netizen <harshit04122008@gmail.com>
2026-08-26 20:53:00 +0530

    files added

---
+++ morphing-rings/src/App.tsx
@@ -0,0 +1,17 @@
+import MorphingRings from "./MorphingRings"
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <MorphingRings />
+    </div>
+  )
+}
+
+export default App
